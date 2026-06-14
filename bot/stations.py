a3d5766ba61358198df8ef0d4ea4e2983110171d
alkhnanyaslam-sys <alkhnanyaslam@gmail.com>
2026-06-14 10:34:16 +0300

    Update stations.py

--- bot/stations.py
+++ bot/stations.py
@@ -8,5 +8,12 @@ STATIONS = {
         "name": "🕌 المسجد النبوي الشريف",
         "url": "https://ws.stream.iqra.tv/hls/madinah/index.m3u8",
         "backup": "https://cdn-streams.tv/haramain/madinah/hd/index.m3u8",
+    },
+    "3": {
+        "name": "📻 إذاعة القرآن الكريم - القاهرة",
+        "url": "http://live.mp3quran.net:9722/",
+        "backup": "http://live.mp3quran.net:8006/",
+    }
+}
 
-STATION_ORDER = ["1", "2"]
+STATION_ORDER = ["1", "2", "3"]
